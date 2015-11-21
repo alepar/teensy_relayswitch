11bdaf0d4004f9b9327cfa511073e3bf4b73032c
Alexey Parfenov <alepar@gmail.com>
2015-11-21 00:55:24 -0800

    turn off the screen when inactive display some dummy relay-switch schedule

--- src/main.cpp
+++ src/main.cpp
@@ -27,87 +27,119 @@ extern "C" int main(void) {
 
 	Selection selection(6);
 
+	uint64_t lastEvent = millis(), curMillis = lastEvent;
+
 	while(1) {
-		uint8_t showSelection = (millis()/125) % 2;
-		uint8_t selected = selection.selected();
+		curMillis = millis();
+
+		uint8_t showSelection = (curMillis/125) % 2;
+		int8_t selected = selection.selected();
 
 		if (btn1.hadClick()) {
 			selection.next();
+			lastEvent = curMillis;
 		}
 		if (btn2.hadClick()) {
-			selection.keep();
-			int _year = year();
-			int _month = month();
-			int _day = day();
-			int _hour = hour();
-			int _minute = minute();
-			int _second = second();
-			switch (selected) {
-				case 0:
-					_year++;
-					break;
-				case 1:
-					_month++;
-					break;
-				case 2:
-					_day++;
-					break;
-				case 3:
-					_hour++;
-					break;
-				case 4:
-					_minute++;
-					break;
-				case 5:
-					_second++;
-					break;
-				default:
-					break;
+			lastEvent = curMillis;
+			if (selected >= 0 ) {
+				selection.keep();
+				int _year = year();
+				int _month = month();
+				int _day = day();
+				int _hour = hour();
+				int _minute = minute();
+				int _second = second();
+				switch (selected) {
+					case 0:
+						_year++;
+						break;
+					case 1:
+						_month++;
+						break;
+					case 2:
+						_day++;
+						break;
+					case 3:
+						_hour++;
+						break;
+					case 4:
+						_minute++;
+						break;
+					case 5:
+						_second++;
+						break;
+					default:
+						break;
+				}
+				setTime(_hour, _minute, _second, _day, _month, _year);
 			}
-			setTime(_hour, _minute, _second, _day, _month, _year);
 		}
 
 		display.clearDisplay();
-		display.setCursor(0, 0);
-		display.print(" ");
 
-		if (selected == 0 && !showSelection) {
-			display.print("    ");
-		} else {
-			display.printf("%04u", year());
-		}
-		display.print("/");
-		if (selected == 1 && !showSelection) {
-			display.print("  ");
-		} else {
-			display.printf("%02u", month());
-		}
-		display.print("/");
-		if (selected == 2 && !showSelection) {
-			display.print("  ");
-		} else {
-			display.printf("%02u", day());
-		}
-		display.print(" ");
+		if (curMillis-lastEvent<15000) {
+			display.setCursor(0, 0);
+			display.print(" ");
 
-		if (selected == 3 && !showSelection) {
-			display.print("  ");
-		} else {
-			display.printf("%02u", hour());
+			if (selected == 0 && !showSelection) {
+				display.print("    ");
+			} else {
+				display.printf("%04u", year());
+			}
+			display.print("/");
+			if (selected == 1 && !showSelection) {
+				display.print("  ");
+			} else {
+				display.printf("%02u", month());
+			}
+			display.print("/");
+			if (selected == 2 && !showSelection) {
+				display.print("  ");
+			} else {
+				display.printf("%02u", day());
+			}
+			display.print(" ");
+
+			if (selected == 3 && !showSelection) {
+				display.print("  ");
+			} else {
+				display.printf("%02u", hour());
+			}
+			display.print(":");
+			if (selected == 4 && !showSelection) {
+				display.print("  ");
+			} else {
+				display.printf("%02u", minute());
+			}
+			display.print(":");
+			if (selected == 5 && !showSelection) {
+				display.print("  ");
+			} else {
+				display.printf("%02u", second());
+			}
+			display.println("\n");
+
+			display.setTextColor(BLACK, WHITE);
+			display.print("1");
+			display.setTextColor(WHITE);
+			display.print(" 00:00:00 - 12:00:00");
+
+			display.setTextColor(WHITE);
+			display.print("2");
+			display.setTextColor(WHITE);
+			display.print(" 00:00:00 - 12:00:00");
+
+			display.setTextColor(BLACK, WHITE);
+			display.print("3");
+			display.setTextColor(WHITE);
+			display.print(" 00:00:00 - 12:00:00");
+
+			display.setTextColor(WHITE);
+			display.print("4");
+			display.setTextColor(WHITE);
+			display.print(" 00:00:00 - 12:00:00");
 		}
-		display.print(":");
-		if (selected == 4 && !showSelection) {
-			display.print("  ");
-		} else {
-			display.printf("%02u", minute());
-		}
-		display.print(":");
-		if (selected == 5 && !showSelection) {
-			display.print("  ");
-		} else {
-			display.printf("%02u", second());
-		}
-		display.println();
+		
 		display.display();
 	}
 }
